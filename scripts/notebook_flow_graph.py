8e443e6e8388fd7520c95972456bb6643afe3440
reina.al-masri <reina.al-masri@universite-paris-saclay.fr>
2026-05-22 14:03:39 +0200

    feat(notebook): expanded cell clusters

--- scripts/notebook_flow_graph.py
+++ scripts/notebook_flow_graph.py
@@ -5,7 +5,7 @@ import re
 NODE_RE = re.compile(r'^\s*"?(?P<id>[^"\s\[]+)"?\s+\[(?P<attrs>.*?)\];', re.MULTILINE | re.DOTALL)
 POS_RE = re.compile(r'pos="(?P<x>[^,"]+),(?P<y>[^"]+)"')
 
-
+# to get positions computed by graphiz
 def get_graphviz_positions(dot_source):
     positions = {}
     for node_match in NODE_RE.finditer(dot_source):
@@ -18,7 +18,7 @@ def get_graphviz_positions(dot_source):
     return positions
 
 
-def build_dot(dico):
+def build_dot(dico, include_subworkflows=False):
     dot = graphviz.Digraph(name="Notebook")
     dot.attr(rankdir="LR")
     dot.attr(
@@ -29,15 +29,60 @@ def build_dot(dico):
     )
     dot.attr("edge")
 
-    for node in dico["nodes"]:
-        dot.node(
-            node["id"],
-            node["name"],
-            shape=node.get("shape", "circle"),
-            fillcolor=node.get("fillcolor", "#eeeeee"),
-        )
+    if include_subworkflows:
+        add_subworkflow_nodes(dot, dico)
+    else:
+        add_nodes(dot, dico["nodes"])
 
-    for edge in dico["edges"]:
-        dot.edge(edge["A"], edge["B"], label=edge.get("label", ""))
+    add_edges(dot, dico["edges"])
 
     return dot.source
+
+
+def build_expanded_dot(dico, include_subworkflows=True):
+    # helper for expanded notebook graphs
+    # e.g. one cell subworkflow becomes a Graphviz cluster box
+    return build_dot(dico, include_subworkflows=include_subworkflows)
+
+
+def add_nodes(dot, nodes):
+    for node in nodes:
+        add_node(dot, node)
+
+
+def add_node(dot, node):
+    dot.node(
+        node["id"],
+        node["name"],
+        shape=node.get("shape", "circle"),
+        fillcolor=node.get("fillcolor", "#eeeeee"),
+    )
+
+
+def add_subworkflow_nodes(dot, dico):
+    grouped = set()
+
+    for sub_id, sub in dico.get("subworkflows", {}).items():
+        # Graphviz clusters draw the visual box around statement nodes
+        # e.g. cluster_cell_0 contains cell_0_stmt_0 and cell_0_stmt_1
+        with dot.subgraph(name=f"cluster_{sub_id}") as c:
+            c.attr(
+                label=sub.get("label", sub_id),
+                color=sub.get("color", "#fefefa"),
+                style="filled",
+            )
+            for node in dico["nodes"]:
+                if node["id"] in sub.get("nodes", []):
+                    add_node(c, node)
+                    grouped.add(node["id"])
+
+    # nodes outside a subworkflow still have to be rendered normally
+    for node in dico["nodes"]:
+        if node["id"] not in grouped:
+            add_node(dot, node)
+
+
+def add_edges(dot, edges):
+    for edge in edges:
+        # edge endpoints already exist as normal nodes or cluster nodes
+        dot.edge(edge["A"], edge["B"], label=edge.get("label", ""))
